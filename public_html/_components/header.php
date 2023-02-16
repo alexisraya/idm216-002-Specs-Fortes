65cb24f9a8ebf6a8da9aa85d2776819606f1dd26
Alexis Raya <aar335@drexel.edu>
2023-02-15 20:01:38 -0500

    :art: cleaning up code files

--- public_html/_components/header.php
+++ public_html/_components/header.php
@@ -16,7 +16,7 @@ $document_title = $page_title . ' | ' . $site_title;
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tiny.cloud/1/cqfi5zr7u3ffymv0qnpgpnuun8v5avkspcekezmg25ooe5kh/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-  <link rel="icon" type="image/x-icon" href="<?php echo site_url(); ?>/dist/images/favicon.svg">
+  <link rel="icon" type="image/x-icon" href="<?php echo site_url(); ?>/_dist/images/favicon.svg">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <link rel="stylesheet" href="<?php echo site_url(); ?>/_dist/styles/style.css">
   <title><?php echo $document_title ; ?></title>
