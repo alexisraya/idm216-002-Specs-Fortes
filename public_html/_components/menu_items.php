697c9c2622d64e7b4f91e4aa3dd26ee998a4ebbd
Alexis Raya <aar335@drexel.edu>
2023-03-13 23:19:16 -0400

    :construction:

--- public_html/_components/menu_items.php
+++ public_html/_components/menu_items.php
@@ -9,28 +9,28 @@
 <div class='menu_category'>
     <h2 class='category_title'>SPECIALS</h2>
     <?php
-        while ($menuItem = mysqli_fetch_array($menuItems)) {
-            if ($menuItem['category'] == 'specials'){
+        $query = "SELECT * FROM menu WHERE menu_category = 'burgers'";
+        $result = mysqli_query($db_connection, $query);
+        while ($menuItem = mysqli_fetch_assoc($result)){
                 $price = '$' . number_format($menuItem['price']/100, 2);
             echo "
-                    <div class='menu_item_container'>
-                        <div class='menu_item_image'>
-                        <img src='http://placehold.it/83x92' alt='Menu Item Image'>
+                <div class='menu_item_container'>
+                    <div class='menu_item_image'>
+                    <img src='http://placehold.it/83x92' alt='Menu Item Image'>
+                    </div>
+                    <div class='menu_item_text'>
+                        <div class='menuItem_title'>
+                            <h3>{$menuItem['name']}</h3>
                         </div>
-                        <div class='menu_item_text'>
-                            <div class='menuItem_title'>
-                                <h3>{$menuItem['name']}</h3>
-                            </div>
-                            <div class='menu_item_description'>
-                                <p>{$menuItem['item_description']}</p>
-                            </div>
-                            <div class='menu_item_price'>
-                                <p>$price</p>
-                            </div>
+                        <div class='menu_item_description'>
+                            <p>{$menuItem['item_description']}</p>
+                        </div>
+                        <div class='menu_item_price'>
+                            <p>$price</p>
                         </div>
                     </div>
-                ";
-            }
+                </div>
+            ";
         }
     ?>
 </div>
@@ -38,39 +38,11 @@
 <div class='menu_category'>
     <h2 class='category_title'>BURGERS</h2>
     <?php
-        while ($menuItem = mysqli_fetch_array($menuItems)) {
-            if ($menuItem['category'] == 'burgers'){
-                $price = '$' . number_format($menuItem['price']/100, 2);
+        $query = "SELECT * FROM menu WHERE menu_category = 'burgers'";
+        $result = mysqli_query($db_connection, $query);
+        while ($menuItem = mysqli_fetch_assoc($result)){
+            $price = '$' . number_format($menuItem['price']/100, 2);
             echo "
-                    <div class='menu_item_container'>
-                        <div class='menuItem_image'>
-                        <img src='http://placehold.it/83x92' alt='Menu Item Image'>
-                        </div>
-                        <div class='menu_item_text'>
-                            <div class='menuItem_title'>
-                                <h3>{$menuItem['name']}</h3>
-                            </div>
-                            <div class='menu_item_description'>
-                                <p>{$menuItem['item_description']}</p>
-                            </div>
-                            <div class='menu_item_price'>
-                                <p>$price</p>
-                            </div>
-                        </div>
-                    </div>
-                ";
-            }
-        }
-    ?>
-</div>
-
-<div class='menu_category'>
-    <h2 class='category_title'>SIDES</h2>
-    <?php
-        while ($menuItem = mysqli_fetch_array($menuItems)) {
-            if ($menuItem['category'] == 'sides'){
-                $price = '$' . number_format($menuItem['price']/100, 2);
-                echo "
                 <div class='menu_item_container'>
                     <div class='menuItem_image'>
                     <img src='http://placehold.it/83x92' alt='Menu Item Image'>
@@ -88,7 +60,35 @@
                     </div>
                 </div>
             ";
-            }
+        }
+    ?>
+</div>
+
+<div class='menu_category'>
+    <h2 class='category_title'>SIDES</h2>
+    <?php
+        $query = "SELECT * FROM menu WHERE menu_category = 'sides'";
+        $result = mysqli_query($db_connection, $query);
+        while ($menuItem = mysqli_fetch_assoc($result)) {
+            $price = '$' . number_format($menuItem['price']/100, 2);
+            echo "
+                <div class='menu_item_container'>
+                    <div class='menuItem_image'>
+                    <img src='http://placehold.it/83x92' alt='Menu Item Image'>
+                    </div>
+                    <div class='menu_item_text'>
+                        <div class='menuItem_title'>
+                            <h3>{$menuItem['name']}</h3>
+                        </div>
+                        <div class='menu_item_description'>
+                            <p>{$menuItem['item_description']}</p>
+                        </div>
+                        <div class='menu_item_price'>
+                            <p>$price</p>
+                        </div>
+                    </div>
+                </div>
+            ";
         }
     ?>
 </div>
